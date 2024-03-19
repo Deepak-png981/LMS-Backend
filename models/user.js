4f210cf8c699f1c59e517d6292056c8168d87a6a
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2024-03-19 23:33:09 +0530

    done

--- models/user.js
+++ models/user.js
@@ -72,7 +72,8 @@ const userSchema = new mongoose.Schema({
         type: String,
         validate: {
             validator: function (v) {
-                return /\d{3}-\d{3}-\d{4}/.test(v)
+                // Allow digits, plus sign, and hyphen
+                return /^[\d+\-]+$/.test(v);
             },
             message: (props) => `${props.value} is not a valid phone number!`
         },
